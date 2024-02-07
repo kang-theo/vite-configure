c736d8dba103b19f2cb27b20119c425668188007
kang-theo <theo.cy.kang@gmail.com>
2024-02-07 21:15:44 +0800

    CSS, JSON support

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,9 @@
 
 <script lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
+import "./assets/app.css"
+import info from "./assets/info.json"
+
 const tsTest: string = "Vite for Vue 3.0"
 
 export default {
@@ -14,6 +17,7 @@ export default {
   },
   mounted() {
     console.log(tsTest)
+    console.log(info.name + " | " + info.website)
   }
 }
 </script>
